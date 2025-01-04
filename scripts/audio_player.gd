20c4d0a6bafd98af841671d80b2598e7cd054b85
Dmitry Valov <d_valov@mail.ru>
2025-01-04 16:50:23 +0600

    Зациклил фоновую музыку

--- scripts/audio_player.gd
+++ scripts/audio_player.gd
@@ -1,10 +1,16 @@
 extends Node
 
+@onready var music_player = $MusicPlayer
+
 var hurt = preload("res://assets/audio/hurt.wav")
 var jump = preload("res://assets/audio/jump.wav")
 var win = preload("res://assets/audio/hurt.wav")
 var coin = preload("res://assets/audio/coin.wav")
 
+func _ready() -> void:
+	music_player.connect("finished", Callable(self,"_on_loop_sound").bind(music_player))
+	music_player.play()
+	
 func play_sfx(sfx_name: String):
 	var stream = null
 	if sfx_name == "hurt":
@@ -29,3 +35,7 @@ func play_sfx(sfx_name: String):
 
 	await asp.finished
 	asp.queue_free()
+	
+func _on_loop_sound(player):
+	music_player.stream_paused = false
+	music_player.play()

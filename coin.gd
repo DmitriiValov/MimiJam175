a18ab88f2d4bc29eb16e1e989463db25e0b6fcf3
Dmitry Valov <d_valov@mail.ru>
2025-01-04 20:30:30 +0600

    Переделал класс spike_ball

--- coin.gd
+++ coin.gd
@@ -3,10 +3,10 @@ extends Area2D
 signal coin_collected
 
 func _ready():
-    body_entered.connect(_on_body_entered)
+	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
-    if body is Player:
-        coin_collected.emit()
-        AudioPlayer.play_sfx("coin")
-        queue_free()
+	if body is Player:
+		coin_collected.emit()
+		AudioPlayer.play_sfx("coin")
+		queue_free()
